b50928bf941286db54abf9a618b3b4b368dd4ee2
bmenchaca <bmenchaca.cca@gmail.com>
2025-01-14 22:06:27 -0600

    feat: card list component created

---
+++ components/CardList.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useRef, useState } from "react";
+
+const CardList: React.FC = () => {
+  return (
+    <div className="flex justify-center bg-striped">
+      <div className="w-full sm:w-4/5 md:w-3/5 bg-gray-100 p-4 m-2 rounded min-h-screen border-2">
+
+      </div>
+    </div>
+  );
+};
+
+export default CardList;
