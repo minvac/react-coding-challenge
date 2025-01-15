0cdc3d97aca107839e94c1e4b5f59b3e4c6f5c6c
bmenchaca <bmenchaca.cca@gmail.com>
2025-01-15 01:25:00 -0600

    feat: update PeopleCard component and refactor fetching logic

--- components/PeopleCard.tsx
+++ components/PeopleCard.tsx
@@ -33,9 +33,10 @@ const PeopleCard: FC<CharacterProps> = ({ description }) => {
   return (
     <div className="p-4 bg-white border border-gray-200 rounded-lg shadow text-left gap-2 min-w-[300px] min-h-[150px] mt-4 sm:min-w-[200px] sm:min-h-[100px] md:min-w-[250px] md:min-h-[120px] lg:min-w-[300px] lg:min-h-[150px]">
       <div className='flex items-center gap-2 mb-2'>
-        {/* <Skeleton circle width={42} height={42} /> */}
-        {/* <img src={person.picture.thumbnail} alt={`${person.name.first} ${person.name.last}`} /> */}
-        {/* <h5 className="text-lg font-bold tracking-tight text-gray-900 dark:text-white">{`${person.name.title} ${person.name.first} ${person.name.last}`}</h5> */}
+        <img src={person.picture.thumbnail} alt={`${person.name.first} ${person.name.last}`} />
+        <h5 className="text-lg font-bold tracking-tight text-gray-900 dark:text-white">
+          {`${person.name.first} ${person.name.last}`}
+        </h5>
       </div>
       <p className="font-normal text-gray-700 dark:text-gray-400">{description}</p>
     </div>
--- hooks/useFetchPeople.ts
+++ hooks/useFetchPeople.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 
 interface PeopleCard {
   name: {
-    title: string;
     first: string;
     last: string;
   };
@@ -12,27 +11,24 @@ interface PeopleCard {
   };
 }
 
-const fetchPerson = async (): Promise<PeopleCard> => {
-  try {
-    const response = await axios.get(`https://randomuser.me/api?results=1`);
-    console.log(response);
-
-    const person = response.data.results[0];
-    return {
-      name: person.name,
-      picture: person.picture,
-    };
-  } catch (error) {
-    console.error("Error fetching person:", error);
-    throw error;
-  }
+const fetchPerson = async (): Promise<PeopleCard[]> => {
+  const response = await axios.get(`https://randomuser.me/api?results=1`);
+  return response.data.results.map((person: any) => ({
+    name: {
+      first: person.name.first,
+      last: person.name.last,
+    },
+    picture: {
+      thumbnail: person.picture.thumbnail,
+    },
+  }));
 };
 
-const useFetchPerson = () => {
-  return useQuery<PeopleCard, Error>({
+const useFetchPeople = () => {
+  return useQuery<PeopleCard[], Error>({
     queryKey: ["person"],
     queryFn: fetchPerson,
   });
 };
 
-export default useFetchPerson;
+export default useFetchPeople;
